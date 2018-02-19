300f68d6a0fb0fcf4e912f1fdc8de1791e6023fb
Iron Mastiff <1760298283@qq.com>
2018-02-19 16:30:06 +0800

    add train with hyperparameters

---
+++ train.py
@@ -0,0 +1,13 @@
+'''Hyperparameters'''
+
+# Size of input image to discriminator
+input_seze = 784
+# Size of latent vector to genorator
+z_size = 100
+# Size of hidden layers in genorator and discriminator
+g_hidden_size = 128
+d_hidden_size = 128
+# Leak factor for leaky ReLU
+alpha = 0.01
+# Smoothing
+smooth = 0.1
