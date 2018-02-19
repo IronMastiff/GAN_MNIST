531c6d18f8e34887a010b90bc104ab2584c5c523
Iron Mastiff <1760298283@qq.com>
2018-02-19 18:01:12 +0800

    import Discriminator and Generator Losses

--- train.py
+++ train.py
@@ -15,7 +15,7 @@ alpha = 0.01
 # Smoothing
 smooth = 0.1
 
-'''------------Build network------------'''
+'''------------Build network-------------'''
 
 tf.reset_default_graph()
 
@@ -29,4 +29,16 @@ g_model = utils.generator( input_z, input_size, n_units = g_hidden_size, alpha =
 d_model_real, d_logits_real = utils.discriminator( input_real, n_units = d_hidden_size, alpha = alpha )
 d_model_fake, d_logits_fack = utils.discriminator( g_model, reuse = True, n_units = d_hidden_size, alpha = alpha )
 
-'''---------------------------------------'''
+
+'''---Discriminator and Generator Losses---'''
+# Calculate losses
+d_loss_real = tf.reduce_mean( tf.nn.sigmoid_cross_entropy_with_logits( logits = d_logits_real,
+                                                                       labels = tf.ones_like( d_logits_real ) * ( 1 - smooth ) ) )
+
+d_loss_fake = tf.reduce_mean( tf.nn.sigmoid_cross_entropy_with_logits( logits = d_logits_fake,
+                                                                       labels = tf.zeros_like( d_logits_fack ) ) )
+
+d_loss = d_loss_real + d_loss_fake
+
+g_loss = tf.reduce_mean( tf.nn.sigmoid_cross_entropy_with_logits( logits = d_logits_fack,
+                                                                  labels = tf.ones_like( d_logits_fack ) ) )
