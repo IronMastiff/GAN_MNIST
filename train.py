4a754b22dc798a5333aab1b5e1f72014368ab0c7
Iron Mastiff <1760298283@qq.com>
2018-03-31 13:32:55 +0800

    Writing the generate picture to the hard disk

--- train.py
+++ train.py
@@ -3,6 +3,7 @@ import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import argparse
+import os
 
 import utils
 
@@ -18,16 +19,32 @@ def parse_args():
     return parser.parse_args()
 
 
+args = parse_args()
+FLAGS = utils.read_config_file( args.config )
+
+if not( os.path.exists( FLAGS.generate_file ) ):
+    os.makedirs( FLAGS.generate_file )
+
+
+'''--------Preprocessing data--------'''
+if( FLAGS.select_label != 'All' ):
+    datas = utils.select_data( mnist, FLAGS.select_label )
+else:
+    datas = mnist.train.images    # shape ( 55000, 784 )
+
+batches = utils.batch_data( datas, FLAGS.batch_size )
+
+
 '''-----------Hyperparameters------------'''
 # Size of input image to discriminator
 input_size = 784
 # Size of latent vector to genorator
 z_size = 100
 # Size of hidden layers in genorator and discriminator
-g_hidden_size = 128
-d_hidden_size = 128
+g_hidden_size = FLAGS.g_hidden_size
+d_hidden_size = FLAGS.d_hidden_size
 # Leak factor for leaky ReLU
-alpha = 0.01
+alpha = FLAGS.alpha
 # Smoothing
 smooth = 0.1
 
@@ -65,7 +82,7 @@ tf.summary.scalar('g_loss', g_loss)
 
 '''---------------Optimizers----------------'''
 # Optimizers
-learning_rate = 0.002
+learning_rate = FLAGS.learning_rate
 
 # Get the trainable_variables, split into G and D parts
 t_vars = tf.trainable_variables()
@@ -76,13 +93,9 @@ d_train_opt = tf.train.AdamOptimizer( learning_rate ).minimize( d_loss, var_list
 g_train_opt = tf.train.AdamOptimizer( learning_rate ).minimize( g_loss, var_list = g_vars )
 
 
-'''--------Print Loss and Image with Tensorboard--------'''
-
-
-
 '''-----------------Traing---------------------'''
-batch_size = 20000
-epoches = 100
+batch_size = FLAGS.batch_size
+epoches = FLAGS.epoches
 samples = []
 # losses = []
 # Only save generator variables
@@ -93,11 +106,10 @@ with tf.Session() as sess:
 
     sess.run( tf.global_variables_initializer() )
     for e in range( epoches ):
-        for i in range( mnist.train.num_examples // batch_size ):
-            batch = mnist.train.next_batch( batch_size )
-
+        for batch in batches:
+            # batch = mnist.train.next_batch( batch_size )
             # Get images, reshape and rescale to pass to D
-            batch_images = batch[0].reshape( ( batch_size, 784 ) )
+            batch_images = batch
             batch_images = batch_images * 2 - 1
 
             # Sample random noise for G
@@ -127,38 +139,12 @@ with tf.Session() as sess:
             utils.generator( input_z, input_size, n_units = g_hidden_size, reuse = True, alpha = alpha),
             feed_dict = {input_z : sample_z} )
 
-        '''--------Add generator picture to tensorboard--------'''
-        tf.summary.image('generater', gen_samples.reshape((-1, 28, 28, 1)))
+
+        gen_image = gen_samples.reshape( ( -1, 28, 28, 1 ) )
+        gen_image = tf.cast( np.multiply( gen_image, 255 ), tf.uint8 )
+        for r in range( gen_image.shape[0] ):
+            with open( FLAGS.generate_file + str(e) + ' ' + str( r ) + '.jpg', 'wb' ) as img:
+                img.write( sess.run( tf.image.encode_jpeg( gen_image[r] ) ) )
 
         samples.append( gen_samples )
         saver.save( sess, './checkpoint/generator.ckpt' )
-
-# Save training generator samples
-with open( 'train_samples.pkl', 'wb' ) as f:
-    pkl.dump( samples, f )
-
-
-'''----------Print Training Loss----------'''
-# fig, ax = plt.subplot()
-# losses = np.array( losses )
-# plt.plot( losses.T[0], label = 'Discriminator' )
-# plt.plot( losses.T[1], label = 'Generator' )
-# plt.title( 'Training Losses' )
-# plt.legend()
-
-
-# '''----------Generator samples from training----------'''
-# def view_samples( epoch, samples ):
-#     fig, axes = plt.subplot( figsize = ( 7, 7 ), nrows = 4, sharey = True, sharex = True )
-#     for ax, img in zip( axes.flatten(), samples[epoch] ):
-#         ax.xaxis.set_visible( False )
-#         ax.yaxis.set_visible( False )
-#         im = ax.imshow( img.reshpae( ( 28, 28 ) ), cmap = 'Greys_r' )
-#     return fig, axes
-#
-# # Load samples from generatro taken while training
-# with open( 'train_samples.pkl', 'rb' ) as f:
-#     samples = pkl.load( f )
-
-if __name__ == '__main__':
-
